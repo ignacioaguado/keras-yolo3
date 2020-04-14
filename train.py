bc90bb1237d9a2427319ed950ab18304dbb3d423
Ignacio Aguado <ignacio@stylesage.co>
2020-04-14 15:03:01 +0200

    missing param

--- train.py
+++ train.py
@@ -87,7 +87,7 @@ def create_training_instances(
 
     return train_ints, valid_ints, sorted(labels), max_box_per_image
 
-def create_callbacks(saved_weights_name, tensorboard_logs, model_to_save, valid_generator):
+def create_callbacks(saved_weights_name, tensorboard_logs, model_to_save, valid_generator, labels):
     makedirs(tensorboard_logs)
     
     early_stop = EarlyStopping(
@@ -123,7 +123,8 @@ def create_callbacks(saved_weights_name, tensorboard_logs, model_to_save, valid_
     )
     valid_map = EvaluationCallback(
         infer_model            = model_to_save,
-        valid_generator        = valid_generator
+        valid_generator        = valid_generator,
+        labels.                = labels
     )
     return [early_stop, checkpoint, reduce_on_plateau, tensorboard, valid_map]
 
